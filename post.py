0212375527d283306b6ea145f782d71d9cbd6f0b
modelerconso <natthanan.wannapha@gmail.com>
2022-03-21 12:38:54 +0700

    natthana edit

--- post.py
+++
@@ -1,7 +0,0 @@
-import database
-
-def insertPost():
-    count = database.myCursor.execute("INSERT INTO post (postId,userId,title,content,date) VALUES ('p004, 'u001', 'สวัสดีครับ','ทักไปทุกวัน ความสัมพันธ์แค่เพื่อน','2020-12-15')")
-    database.mySql.commit()
-    database.myCursor.close()
-    database.mySql.close()
